a41690b25e5c792c1fba65a4615c7efc641c031c
Hannah <hdeters07@gmail.com>
2015-05-09 15:40:40 -0400

    more efficient version

--- demon_words.py
+++ demon_words.py
@@ -1,8 +1,7 @@
 import game_functions as gf
 import random
 
-#word_list = ["bird", "calf", "river", "iriri", "chime", "crank", "stream","begin","iowan","imide","igrii", "kneecap",  "cookbook",
-#              "language", "sneaker", "algorithm", "integration", "brain"]
+#word_list = ["ally", "beta", "cool", "deal", "else", "flew", "good", "hope", "ibex"]
 
 word_list = []
 guess = ""
@@ -51,52 +50,46 @@ def is_guess_in_word(word_list,guess):
             yes_group.append(word)
         else:
             no_group.append(word)
-    if len(yes_group) > len(no_group):
-        return yes_group, True
-    else:
-        return no_group, False
+    return yes_group,no_group
 
-def sort_group(maxlen,sortlist):
-    group = []
-    for pair in sortlist:
-        if len(pair[1]) == maxlen:
-            group.append(pair)
-    return group
+def find_words_with_index(dict_with_words,idx):
+    list_of_words = []
+    #Returns a list of tuples with (word, indexes where guess is found)
+    word_index_tuples = sorted(dict_with_words.items(), key=lambda x:(x[1]), reverse=True)
+    for pair in word_index_tuples:
+        if str(pair[1]) == str(idx):
+            list_of_words.append(pair[0])
+    return list_of_words
 
 def choose_new_group(working_group,guess):
-    spots_dict = {}
-    position_counts = {}
-    word_groups = []
 
-    for word in working_group:
-        spots = [i for i, letter in enumerate(word) if letter == guess]
-        spots_dict[word] = spots
+    if len(working_group) == 0:
+        return (0,0), []
+    else:
+        spots_dict = {}
+        positionings = {}
+        sorted_positionings = []
 
-    sorted_positions = sorted(spots_dict.items(), key=lambda x:len(x[1]), reverse=False)
+        for word in working_group:
+            spots = [i for i, letter in enumerate(word) if letter == guess]
+            spots_dict[word] = spots
 
-    first_pair = sorted_positions[0]
-    maxlen = len(first_pair[1])
-    while maxlen > 0:
-        group = sort_group(maxlen,sorted_positions)
-        word_groups.append(group)
-        maxlen -= 1
 
-    maxset = 0
-    for set in word_groups:
-        if len(set) > maxset:
-            largest_group = set
-            maxset = len(set)
-    return largest_group
+        for key in spots_dict:
+            idxs = str(spots_dict[key])
+            if idxs in positionings:
+                positionings[idxs] += 1
+            else:
+                positionings[idxs] = 1
 
-def choose_best_position(big_group):
-    display_options = []
-    sorted_group = sorted(big_group, key=lambda x:x[1], reverse=True)
-    best_position = sorted_group[0]
-    position = best_position[1]
-    for group in sorted_group:
-        if group[1] == position:
-            display_options.append(group)
-    return display_options
+        #returns a list of tuples with (idx of letters,how many times that idx appears) from most to least
+        sorted_positionings = sorted(positionings.items(), key=lambda x:(x[1]), reverse=True)
+
+        largest_occurence = sorted_positionings[0]
+
+        list_of_words = find_words_with_index(spots_dict, largest_occurence[0])
+
+        return largest_occurence,list_of_words
 
 def ending(last_word,guessed_letters,guesses,guess):
     while guesses > 0:
@@ -116,57 +109,48 @@ def ending(last_word,guessed_letters,guesses,guess):
             return True
     return False
 
-def create_blank_tuple(length):
-    string = ""
-    while length > 0:
-        string = string + " "
-        length -= 1
-    blank_tup = (string,1)
-    return blank_tup
-
 def main_hard():
     guessed_letters = []
     word_length = get_length()
     guesses = get_guesses()
-    in_word = False
-    last_word = False
-    print_this = create_blank_tuple(word_length)
-    display_options = find_initial_words(word_list,word_length)
+    guess = ''
 
-    while guesses > 0 and gf.still_playing(print_this,guessed_letters):
+    initial_options = find_initial_words(word_list,word_length)
 
-        if last_word:
-            win = ending(the_word,guessed_letters,guesses,guess)
+    while guesses > 0:
+
+        if len(initial_options) == 1:
+            win = ending(initial_options[0],guessed_letters,guesses,guess)
             if win:
                 gf.play_again()
             else:
                 print("You lose, the word was {}".format(the_word))
                 gf.play_again()
-
-        guess = gf.ask_letter(guessed_letters)
-        working_group, in_word = is_guess_in_word(display_options,guess)
-
-        if in_word:
-            guessed_letters.append(guess)
-            largest_group = choose_new_group(working_group,guess)
-            display_tuples = choose_best_position(largest_group)
-            print_this = random.choice(display_tuples)
-            gf.display_word(guessed_letters, print_this[0])
-            print("\n{} is in the word, guesses left:{}".format(guess,guesses))
-            display_options = []
-            for pair in display_tuples:
-                display_options.append(pair[0])
-
         else:
-            guesses -= 1
-            gf.display_word(guessed_letters, print_this[0])
-            print("\n{} is not in the word, guesses left:{}".format(guess,guesses))
-            if len(working_group) == 1:
-                last_word = True
-                the_word = working_group[0]
-            display_options = working_group
-    print("You lose, the word was {}".format(random.choice(display_options)))
-    gf.play_again()
+
+            guess = gf.ask_letter(guessed_letters)
+            guessed_letters.append(guess)
+
+            list_letter_in, list_letter_not_in = is_guess_in_word(initial_options,guess)
+            most_frequent_index,words_with_index = choose_new_group(list_letter_in,guess)
+
+            if (most_frequent_index[1]) > len(list_letter_not_in):
+                print_this = random.choice(words_with_index)
+                initial_options = words_with_index
+                print("\n{} is in the word, guesses left:{}".format(guess,guesses))
+                gf.display_word(guessed_letters,print_this)
+            else:
+                print_this = random.choice(list_letter_not_in)
+                initial_options = list_letter_not_in
+                guesses -= 1
+                print("\n{} is not in the word, guesses left:{}".format(guess,guesses))
+                gf.display_word(guessed_letters,print_this)
+
+            print(print_this)
+
+    if guesses == 0:
+        print("You lose, the word was {}".format(initial_options[0]))
+        gf.play_again()
 
 if __name__ == "__main__":
     main_hard()
